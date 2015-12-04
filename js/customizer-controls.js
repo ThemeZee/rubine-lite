269eb9fe82b8a72bbdb1d256d27d3ebada819cb0
Thomas W <netzberufler@googlemail.com>
2015-12-04 12:00:49 +0100

    added new upgrade control and google analytics campaign urls

--- js/customizer-controls.js
+++ js/customizer-controls.js
@@ -46,7 +46,7 @@
 		
 		setTimeout(function () {
 			$('#accordion-panel-rubine_options_panel .control-panel-content').append(content);
-		}, 200);
+		}, 2000);
 
 		// Remove accordion click event
 		$('.rubine-theme-links a').on('click', function(e) {
